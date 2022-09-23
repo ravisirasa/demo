a59a6ee91618133f3617a814eb510f09c3bbe697
Git-Demo <abc.pwc.com>
2022-09-23 15:47:40 +0530

    reset

--- HW.java
+++ HW.java
@@ -1,6 +1,20 @@
 public class HW{
 public static void main(String[] args){
+System.out.println("demo1");
+System.out.println("demo2");
+System.out.println("demo3");
+System.out.println("demo");
+System.out.println("demo5");
+System.out.println("demo");
+System.out.println("demo7");
+System.out.println("demo");
 System.out.println("Hello World1");
-System.out.println("Master");
+System.out.println("demo");
+System.out.println("Hello World1");
+System.out.println("demo");
+System.out.println("Hello World1");
+System.out.println("demo");
+System.out.println("Hello World1");
+System.out.println("demo");
 }
 }
